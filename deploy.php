51a717467940bb227033481cbed949aa28e6b0db
合同会社FGC <goudoukaishafgc@iMac-215inch.local>
2019-11-02 20:26:09 +0900

    add-deployer

---
+++ deploy.php
@@ -0,0 +1,45 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony4.php';
+
+// Project name
+set('application', 'vlogger');
+
+// Project repository
+set('repository', 'git@github.com-gs:fgc0415/Vlogger.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+// add('shared_files', []);
+// add('shared_dirs', []);
+
+// Writable dirs by web server
+// add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('fgc.mixh.jp')
+    ->user('lbqhvbsc')
+    ->hostname('fgc.mixh.jp')
+    ->port(22)
+    ->stage('staging')
+    ->set('branch', 'master')
+    ->set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --no-dev')
+    ->set('deploy_path', '~/public_html/{{application}}');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'database:migrate');
