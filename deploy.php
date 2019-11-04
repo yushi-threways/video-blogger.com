c74a4217bc9c664630897834bd34be7f67f95461
合同会社FGC <goudoukaishafgc@iMac-215inch.local>
2019-11-05 02:29:53 +0900

    deployファイルを編集

--- deploy.php
+++ deploy.php
@@ -38,6 +38,12 @@ host('fgc.mixh.jp')
     
 // Tasks
 
+task('change_cwd', function () {
+    $subdir = get('release_path') . DIRECTORY_SEPARATOR . 'webapp';
+    set('release_path', $subdir);
+    run('cd {{release_path}}');
+  });
+  
 task('build', function () {
     run('cd {{release_path}} && build');
 });
@@ -45,6 +51,8 @@ task('build', function () {
 task('deploy:assets:install', function () {
     run('{{bin/php}} {{bin/console}} assets:install {{console_options}} {{release_path}}/public');
 })->desc('Install bundle assets');
+
+after('deploy:update_code', 'change_cwd');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
