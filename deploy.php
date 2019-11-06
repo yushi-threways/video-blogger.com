bd23f7ff8fa42550ba08800840b7539512769232
合同会社FGC <goudoukaishafgc@iMac-215inch.local>
2019-11-06 20:45:48 +0900

    デプロイファイルをcommit

--- deploy.php
+++ deploy.php
@@ -4,10 +4,10 @@ namespace Deployer;
 require 'recipe/symfony4.php';
 
 // Project name
-set('application', 'Vlogger');
+set('application', 'vlogger');
 
 // Project repository
-set('repository', 'git@github.com:fgc0415/Vlogger.git');
+set('repository', 'git@github.com:fgc0415/vlogger.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
